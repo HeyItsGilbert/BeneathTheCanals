d1ee7ede92e3b7ef8a1179995b066782520bc19d
Gilbert Sanchez <me@gilbertsanchez.com>
2021-08-26 17:14:40 -0700

    Update BeneathTheCanals.psm1

--- BeneathTheCanals/BeneathTheCanals.psm1
+++ BeneathTheCanals/BeneathTheCanals.psm1
@@ -1,8 +1,8 @@
 # Dot source public/private functions
 $classes = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Classes/*.ps1') -Recurse -ErrorAction Stop)
 $public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
-foreach ($import in @($classes + $public + $private)) {
+#$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
+foreach ($import in @($classes + $public)) {
     try {
         . $import.FullName
     } catch {
